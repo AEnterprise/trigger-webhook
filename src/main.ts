9ce0ac530e68de916dee1bedea5739c1d051155b
AEnterprise <aenterprise2@gmail.com>
2019-08-25 17:12:05 +0200

    make curl wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        const CURL_ARGS = core.getInput('CURL_ARGS');
+        const METHOD = core.getInput('METHOD');
+        const URL = core.getInput('URL');
+        const options = {
+            listeners: {
+                stdout: (data: Buffer) => {
+                    core.debug(data.toString());
+                },
+                stderr: (data: Buffer) => {
+                    core.error(data.toString());
+                }
+            },
+            failOnStdErr: true
+        };
+        const args = [`-X ${METHOD}`, `"${URL}"`];
+        CURL_ARGS.split(/(?= --?[A-z]+ ["'])(?: )/g).forEach((part) => args.push(part));
+        await exec.exec("curl", args, options);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
